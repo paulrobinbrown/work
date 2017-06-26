ac5e7a5ca2f079fc16e4c6118c70646d636adfa6
paulrobinbrown <iamatrojanhorse@gmail.com>
2017-06-26 09:16:24 +0100

    Add project files.

---
+++ SharePoint Opener for VSCode/Main.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SharePoint_Opener_for_VSCode
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(502, 20);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://adepteqltd.sharepoint.com/sites/pbtest/sitepages";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(214, 55);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(99, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open VS Code";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(12, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(502, 17);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 86);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SPOpener for VSCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ SharePoint Opener for VSCode/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharePoint_Opener_for_VSCode
+{
+    public partial class Main : Form
+    {
+        const string VSCodePath = @"C:\Program Files (x86)\Microsoft VS Code\code.exe";
+
+        // Import DLL to set Cookies in IE
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool InternetSetCookie(string lpszUrlName, string lpszCookieName, string lpszCookieData);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnOpen_Click(object sender, EventArgs e)
+        {
+            var sharepointUri = new Uri(txtUrl.Text);
+
+            lblStatus.Text = "Getting cookies...";
+
+            await RunAsync(sharepointUri, "paul.brown@adepteq.com", "Prb*123.", false, false);
+
+            if (SpoAuthUtility.Current != null)
+            {
+                //txtStatus.AppendText("Setting Cookies in OS...");
+                try
+                {
+                    // Create the cookie collection object for sharepoint URI
+                    CookieCollection cookies = SpoAuthUtility.Current.cookieContainer.GetCookies(sharepointUri);
+
+                    // Extract the base URL in case the URL provided contains nested paths (e.g. https://contoso.sharepoint.com/abc/ddd/eed)
+                    // The cookie has to be set for the domain (contoso.sharepoint.com), otherwise it will not work
+                    String baseUrl = sharepointUri.Scheme + "://" + sharepointUri.Host;
+
+                    if (InternetSetCookie(baseUrl, null, cookies["FedAuth"].ToString() + "; Expires = " + cookies["FedAuth"].Expires.AddMinutes(0).ToString("R")))
+                    {
+                        if (InternetSetCookie(baseUrl, null, cookies["rtFA"].ToString() + "; Expires = " + cookies["rtFA"].Expires.AddMinutes(0).ToString("R")))
+                        {
+                            //txtStatus.AppendText("[OK]. Expiry = " + cookies["FedAuth"].Expires.AddMinutes(0).ToString("R"));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = ("[ERROR setting Cookies]:" + ex.Message);
+                }
+            }
+
+            var webDAVPath = txtUrl.Text;
+
+            // Change all back-slashes to forward-slashes
+            webDAVPath = webDAVPath.Replace("/", "\\");
+
+            // Remove protocol
+            webDAVPath = webDAVPath.Replace("https:", string.Empty);
+
+            // Insert @SSL\DavWWWRoot
+            webDAVPath = webDAVPath.Replace(".sharepoint.com", ".sharepoint.com@SSL\\DavWWWRoot");
+
+            lblStatus.Text = string.Format("Opening '{0}' in VS Code...", webDAVPath);
+
+            // Start VS Code with the SharePoint Path
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = Path.GetFileName(VSCodePath);
+            processInfo.WorkingDirectory = Path.GetDirectoryName(VSCodePath);
+            processInfo.Arguments = string.Format("-n {0}", webDAVPath);
+
+            Process.Start(processInfo);
+        }
+
+        static async Task RunAsync(Uri sharepointUri, string username, string password, bool useIntegratedAuth, bool verbose)
+        {
+            await SpoAuthUtility.Create(sharepointUri, username, password, useIntegratedAuth, verbose);
+        }
+    }
+}
